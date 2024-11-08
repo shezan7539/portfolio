97259ea8f3f7e6b2c17fa93a6ff10abd9327864b
shezan7539 <75947263+shezan7539@users.noreply.github.com>
2024-11-09 01:32:06 +0600

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,37 +1,37 @@
-const logotext = "JOHN";
+const logotext = "SHEZAN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Shezan",
+    description: "Tech enthusiast focused on innovation and impact, combining technical skill and creativity to drive purposeful digital solutions.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m MD Mizanur Rahman (Shezan)",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Student • Coder",
+        second: "Gamer • Social Media Expert",
+        third: "Based In Naogaon Sadar",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "I'm MD Mizanur Rahman, a dedicated tech enthusiast with a strong focus on digital innovation and purposeful design. I bring together technical expertise and creativity to build impactful solutions, constantly pushing boundaries to make a meaningful difference. This portfolio reflects my commitment to excellence and growth in the tech world.",
+    your_img_url: "https://ibb.co.com/ynvP7R9",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "About Myself",
+    aboutme: "I'm Mizanur Rahman, a tech enthusiast focused on creating innovative digital solutions. I combine technical skills with creativity to design impactful projects. This portfolio reflects my commitment to continuous growth and excellence in the tech field.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "HSC",
+        where: "NAOGAON ASTAN MOLLAH COLLEGE, NAOGAON",
+        date: "2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "SSC",
+        where: "PIROJPUR B.L HIGH SCHOOL, NAOGAON",
+        date: "2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
+        jobtitle: "JSC",
+        where: "PIROJPUR B.L HIGH SCHOOL, NAOGAON",
         date: "2019",
     },
 ];
@@ -60,85 +60,85 @@ const skills = [{
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Loading...........",
     },
     {
         title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Haha Check Your Internet.",
     },
     {
         title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Haha, I Apolozige I Don't Have These Skills. ",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/vsH8M7d",
+        description: "Sunhera Resturant, Naogaon Sadar",
+        link: "https://www.facebook.com/share/15AzKwx5Cd/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/zRSh0dC",
+        description: "With Piyash Buddy, Naogaon Sadar",
+        link: "https://www.facebook.com/share/1AjDGx4vTo/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/xfbp6LC",
+        description: "First Time In Rajshahi. Captured By Farid Vai",
+        link: "https://www.facebook.com/share/15e4haj6dg/",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/hcJ3Dxj",
+        description: "Shapno Puri, Dinajpur",
+        link: "https://www.facebook.com/share/1CqUxPc9tJ/",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/23F87zf",
+        description: "Barin Sky Loudge, Naogaon Sadar",
+        link: "https://www.facebook.com/share/1EzRbsQ9Ya/",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/2tmD4hz",
+        description: "Memories With Adib",
+        link: "https://www.facebook.com/share/18wUxyTA8X/",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/0Jq5HMn",
+        description: "A Memorable Trip With Close Friends",
+        link: "https://www.facebook.com/share/1WTaF6WL84/",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/MGBFdmv",
+        description: "Hashaigari Bill, Naogaon Sadar",
+        link: "https://www.facebook.com/photo.php?fbid=1685975578862894&set=pb.100023514041448.-2207520000&type=3",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/Yck08x1",
+        description: "Sabuz Chemistry Care 2024 Batch",
+        link: "https://www.facebook.com/share/1DdAQxkLr1/",
     },
     {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/3FMx6zT",
+        description: "Haha T71 x Me",
+        link: "https://www.facebook.com/share/158EmpYTU1/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/37kRY43",
+        description: "Captured By Utshob Vai",
+        link: "https://www.facebook.com/share/p/12B8g9jGyab/",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://ibb.co.com/Z6NPmnD",
+        description: "Old One",
+        link: "https://www.facebook.com/photo.php?fbid=1420841398709648&set=pb.100023514041448.-2207520000&type=3",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "shezan@duck.com",
+    YOUR_FONE: "+8801741847759",
+    description: "Feel free to reach out for collaboration, inquiries, or networking opportunities. I’m always open to connecting with like-minded professionals and exploring new projects. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,10 +147,10 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/shezan7539/",
+    facebook: "https://facebook.com/mdmizanurrahmanshezan/",
+    linkedin: "https://linkedin.com/mdmizanurrahmanshezan/",
+    twitter: "https://twitter.com/shezan7539/",
 };
 export {
     meta,
